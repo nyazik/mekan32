e998f23820ff0d7589a51510718d7399b94df372
Nazik <nyaziktenisistka@gmail.com>
2018-11-03 17:18:04 +0300

    model degistirme

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,9 +1,72 @@
-const anaSayfa=function(req, res){
-    res.render('mekanlar-liste', {'title':'Anaysayfa'});
+const anaSayfa=function(req, res, next){
+    res.render('mekanlar-liste', {
+        'baslik': 'Anasayfa', 
+        'sayfaBaslik':{
+            'siteAd': 'Mekan32', 
+            'aciklama': 'Isparta civarındaki mekanları keşfedin!'
+        },
+        'mekanlar':[
+            {
+                'ad':'Starbucks',
+                'adres':'Centrum Garden',
+                'puan': '3',
+                'imkanlar': ['kahve', 'çay', 'pasta'],
+                'mesafe': '10 km'
+            },
+            {
+                'ad':'Gloaria Jeans',
+                'adres':'Iyash park',
+                'puan': '3',
+                'imkanlar': ['kahve', 'çay', 'pasta'],
+                'mesafe': '5 km'
+            }
+        ]
+    });
 }
 
+
 const mekanBilgisi=function(req, res){
-    res.render('mekan-detay', {'title':'Mekan Bilgisi'});
+    res.render('mekan-detay', {
+        'baslik':'Mekan Bilgisi',
+        'sayfaBaslik':'Starbucks', 
+        'mekanBilgisi':{
+            'ad':'Starbucks',
+            'adres':'Centrum Garden',
+            'puan': '3',
+            'imkanlar': ['kahve', 'çay', 'pasta'],
+            'koordinatlar':{
+                'enlem':37.781885,
+                'boylam':30.566034
+            },
+            'saatler':[
+                {
+                    'gunler':'Pazartesi-Cuma',
+                    'acilis':'7:00',
+                    'kapanis':'23:00',
+                    'kapali':false
+                },
+                {
+                    'gunler':'Cumartesi',
+                    'acilis':'9:00',
+                    'kapanis':'22:30',
+                    'kapali':false
+                },
+                {
+                    'gunler':'Pazar',
+                    'kapali':true
+                }
+            ],
+            'yorumlar':[
+                {
+                    'yorumYapan':'Asim Sinan Yuksel',
+                    'puan': '3',
+                    'tarih':'18 Ekim 2017',
+                    'yorumMetni':'Kahveleri cok guzel'
+                }
+            ]
+        }
+    });
+    
 }
 
 const yorumEkle=function(req, res){
@@ -15,3 +78,7 @@ module.exports={
     mekanBilgisi,
     yorumEkle,
 };
+
+
+
+
